036bb38b098801225714165e70e7cc4e9bb81a41
KNK <knkcool2@gmail.com>
2021-06-10 19:30:06 +0530

    retrofit and moshi implemented no error

--- app/src/main/java/com/nithesh/wordie/wordlist/WordListViewModel.kt
+++ app/src/main/java/com/nithesh/wordie/wordlist/WordListViewModel.kt
@@ -1,10 +1,20 @@
 package com.nithesh.wordie.wordlist
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nithesh.wordie.network.wordService
+import kotlinx.coroutines.launch
+
+
+private val TAG: String = WordListViewModel::class.java.simpleName
+
 
 class WordListViewModel : ViewModel() {
+
+
     private val _text = MutableLiveData<String>()
     val text: LiveData<String>
         get() = _text
@@ -12,7 +22,17 @@ class WordListViewModel : ViewModel() {
     init {
         _text.value = "viewModel created"
     }
-    fun changeText(){
-        _text.value = "text changed "
+
+
+    fun getWordList() {
+        viewModelScope.launch {
+            try {
+                val list = wordService.getWordListAsync("apple")
+                _text.value = list.size.toString()
+            } catch (t: Throwable) {
+                Log.e(TAG, "getWordList: $t")
+                _text.value = "error $t"
+            }
+        }
     }
 }
