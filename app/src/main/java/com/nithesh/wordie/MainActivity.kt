7f2b21514f4b452f601f162ea4b50636bce5f427
KNK <knkcool2@gmail.com>
2021-06-13 13:09:09 +0530

    -search view implemented -safe args added(gradle, plugin) -navigation with safe args added -theme colors changed

--- app/src/main/java/com/nithesh/wordie/MainActivity.kt
+++ app/src/main/java/com/nithesh/wordie/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.nithesh.wordie
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.nithesh.wordie.databinding.ActivityMainBinding
+import com.nithesh.wordie.wordlist.WordListFragmentDirections
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -23,6 +30,50 @@ class MainActivity : AppCompatActivity() {
         navController = navHostFragment.navController
         val appBarConfiguration = AppBarConfiguration(navController.graph)
         binding.toolbar.setupWithNavController(navController, appBarConfiguration)
-        setSupportActionBar(binding.toolbar)
+        binding.toolbar.inflateMenu(R.menu.search_view)
+        val menuItem = binding.toolbar.menu.findItem(R.id.search_view)
+        val searchView = menuItem.actionView as SearchView
+        configureSearchView(searchView)
+//        menuItem.setOnActionExpandListener(object: MenuItem.OnActionExpandListener{
+//            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
+//                binding.toolbar.title = null
+//                binding.invalidateAll()
+//                return true
+//            }
+//
+//            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+//                binding.toolbar.title = getString(R.string.app_name)
+//                binding.invalidateAll()
+//                return true
+//            }
+//        })
     }
+
+    private fun configureSearchView(searchView: SearchView) {
+        searchView.apply {
+            searchView.findViewById<ImageView>(R.id.search_button).setImageDrawable(null)
+            queryHint = getString(R.string.query_hints)
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    Toast.makeText(
+                        this@MainActivity,
+                        query ?: "empty",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    val imm = getSystemService(
+                        Context.INPUT_METHOD_SERVICE
+                    ) as InputMethodManager
+                    imm.hideSoftInputFromWindow(windowToken, 0)
+                    navController.navigate(WordListFragmentDirections
+                        .actionWordListFragmentToSearchFragment())
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    return false
+                }
+            })
+        }
+    }
+
 }
